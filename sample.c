d8ffe027d7c93029962910b558ed89683f503eb9
Abdullah Khilji <abdullahkhilji.nits@gmail.com>
2020-02-26 13:53:28 +0530

    6th

---
+++ sample.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+/* comment 2 */
+int main()
+{
+	// comment
+	// sdfsdf
+	// sdfsdfs
+	// sdfsdf
+	printf("Hello World test1\n");
+	return 0;
+}
+// comment 2 
---
+++ sample2.c
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+					printf("Hello World test1\n");
+	return 0;
+}
+// comment 2 
